7e5cfa1b0135e3fa6232c51b85a9dcb94717a981
Jose Acevedo <acevedoflores.jose@gmail.com>
2013-06-18 17:54:03 -0400

    Music play test works

--- src/msrp/aida/mediaplaybacktest/MainActivity.java
+++ src/msrp/aida/mediaplaybacktest/MainActivity.java
@@ -1,15 +1,93 @@
 package msrp.aida.mediaplaybacktest;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.database.Cursor;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	
+	private MediaPlayer player;
+	private TextView tv;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		player = new MediaPlayer();
+		tv = (TextView) findViewById(R.id.tvMain);
+		
+		ContentResolver cr = this.getContentResolver();
+		
+		Cursor cu = cr.query(android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, 
+				null, null, null, null);
+		if(cu == null)
+		{
+			System.out.println("Error in media query");
+		}
+		else if(!cu.moveToFirst())
+		{
+			System.out.println("No music on phone");
+		}
+		else
+		{
+			ArrayList<Long> ids = new ArrayList<Long>(); 
+			ArrayList<String> title = new ArrayList<String>();
+			int titleColumn = cu.getColumnIndex(android.provider.MediaStore.Audio.Media.TITLE);
+		    int idColumn = cu.getColumnIndex(android.provider.MediaStore.Audio.Media._ID);
+		    int isMusicCol = cu.getColumnIndex(android.provider.MediaStore.Audio.Media.IS_MUSIC);
+		    do {
+		       long thisId = cu.getLong(idColumn);
+		       String thisTitle = cu.getString(titleColumn);
+		       String isMusic = cu.getString(isMusicCol);
+		       if(isMusic.equals("1"))
+		       {
+		    	   ids.add(thisId);
+		    	   title.add(thisTitle);
+		       }
+		       
+		    } while (cu.moveToNext());
+		    
+		    int rand = new Random().nextInt(ids.size());
+		    long thisId = ids.get(rand);
+		    String songTitle = title.get(rand);
+		    
+		    Uri songUri = ContentUris.withAppendedId(android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, thisId);
+		    
+		    player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+		    
+		    try {
+		    	tv.setText(songTitle);
+				player.setDataSource(getApplicationContext(), songUri);
+				player.prepare();
+				player.start();
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalStateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		    
+		}
 	}
 
 	@Override
@@ -19,4 +97,30 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	@Override
+	protected void onPause() {
+		if(player != null && player.isPlaying())
+			player.pause();
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		if(player != null)
+			player.start();
+		super.onResume();
+	}
+
+	@Override
+	protected void onDestroy() {
+		player.stop();
+		player.release();
+		player = null;
+		Log.d("debug", "Destroyed");
+		super.onDestroy();
+	}
+
+	
+	
+	
 }
